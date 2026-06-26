4be7c50e6dc0fd962c25b444d95926b9ba4f81a8
Mohana Devaraj <mohanadevaraj2005@gmail.com>
2026-06-26 15:43:14 +0530

    Moved MongoDB credentials to .env and added .gitignore

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb://ecommerceadmin:ecommerce123@ac-z5dygpe-shard-00-00.c6pxojk.mongodb.net:27017,ac-z5dygpe-shard-00-01.c6pxojk.mongodb.net:27017,ac-z5dygpe-shard-00-02.c6pxojk.mongodb.net:27017/ecommerce?ssl=true&replicaSet=atlas-92o1ea-shard-0&authSource=admin&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
   } catch (error) {
